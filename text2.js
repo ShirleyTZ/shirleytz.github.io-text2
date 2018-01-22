ea84ba05a25cf5949241434f9f0bd9dd342790a6
ShirleyTZ <553728720@qq.com>
2018-01-22 23:02:13 +0800

    跟新

--- text2.js
+++ text2.js
@@ -1,67 +1,42 @@
-function moveElement(elementID,final_x,final_y,interval) {
-  var elem = documentById(elementID);
-  if (elem.movement) {
-  	clearTimeout(elem.movement);;
-  }
-  if (!elem.style.left) {
-  	elem.style.left = "0px";
-  }
-  if (!elem.style.top) {
-  	elem.style.top = "0px";
-  }
-  var xpos = parseInt(elem.style.left);
-  var ypos = parseInt(elem.style.top);
-  if (xpos == final_x && ypos == final) {
-  	return true;
-  }
-  if (xpos < final_x) {
-  	var dist = Math.ceil((final_x - xpos)/10);
-  	xpos = xpos + dist;
-  }
-  if (xpos > final_x) {
-  	var dist = Math.ceil((xpos - final_x)/10);
-  	xpos = xpos - dist;
-  }
-  if (ypos < final_y) {
-    var dist = Math.ceil((final_y - ypos)/10);
-    ypos = ypos + dist;
-  }
-  if (ypos >final_y) {
-  	var dist = Math.ceil((ypos - final_y)/10);
-  	ypos = ypos - dist;
-  }
-  elem.style.left = xpos + "px";
-  elem.style.top = ypos + "px";
-  var repeat = "moveElement('"+elementID"',"+final_x+","+final_y+","+interval+")";
-  elem.movement = setTimeout(repeat,interval);
+var box = document.getElementById("spellImgBox");
+var imgwidth = document.getElementsByClassName("spellImg");
+var nums = document.getElementById("rightbottombutton").getElementsByTagName("li");
+var i = 0;
+auto();
+nums[0].style.cssText = "brackground-color:grey;";
+function auto(){
+  time = setInterval(function(){
+    i++;
+    if(i <= 4) {
+      box.style.left = -i*1226 + "px";
+      nums[i].style.cssText = "background-color:grey;";
+      nums[i -1].style.cssText = "background:none;";
+    } else {
+      box.style.left = "0px";
+      nums[4].style.cssText = "brackground:none;";
+      nums[0].style.cssText = "brackground-color:grey;";
+      i = 0;
+    }
+    console.log(i);
+  },3000)
 }
 
-function prepareSlidesshow() {
-  var rightbottombutton = document.getElementByIdName("rightbottombutton");
-  var links = rightbottombutton.getElementsByTagName("a");
-  var backgroundimg = ducoment.getElementByIdName("backgroundimg");
-  for (var i = 0; i < links.length; i++) {
-	links[i].onmouseover = function() {
-	  var destination = this.getAttribute("data_name");
-	  if (destination == li1){
-		moveElement("backgroundimg",0,0,5);
-	  }
-	  if (destination == li2) {
-		moveElement("backgroundimg",-1226,0,5);
-	  }
-	  if (destination == li3) {
-		moveElement("backgroundimg",-2452,0,5);
-	  }
-	  if (destination == li4) {
-		moveElement("backgroundimg",-3678,0,5);
-	  }
-	  if (destination == li5) {
-		moveElement("backgroundimg",-4904,0,5);
-	  }
-	}
+
+for(var j = 0; j <= 4; j++) {
+  imgwidth[j].index = j;
+  nums[j].index = j;
+  nums[j].onmouseover = function() {
+    this.style.cssText = "background:grey;";
+    this.onmouseout = function() {
+      this.style.cssText = "background:none;";
+    }
+  }
+  nums[j].onlick = function() {
+    clearInterval(time);
+    t = this.index;
+    box.style.left = -t*1226 + "px";
+    i = t;
+    auto();
+    consolo.log(i);
   }
 }
-
-prepareSlidesshow();
-
-
