8163ce7c26243e5c44293145e324a55dc2696fa5
ShirleyTZ <553728720@qq.com>
2018-01-23 16:01:38 +0800

    炸了

--- text2.js
+++ text2.js
@@ -3,7 +3,9 @@ var imgwidth = document.getElementsByClassName("spellImg");
 var nums = document.getElementById("rightbottombutton").getElementsByTagName("li");
 var i = 0;
 auto();
+buttontrans();
 nums[0].style.cssText = "brackground-color:grey;";
+
 function auto(){
   time = setInterval(function(){
     i++;
@@ -18,25 +20,68 @@ function auto(){
       i = 0;
     }
     console.log(i);
-  },3000)
+  },4000)
 }
 
 
-for(var j = 0; j <= 4; j++) {
-  imgwidth[j].index = j;
-  nums[j].index = j;
-  nums[j].onmouseover = function() {
-    this.style.cssText = "background:grey;";
-    this.onmouseout = function() {
-      this.style.cssText = "background:none;";
+function buttontrans() {
+  var numsbutton = document.getElementById("rightbottombutton");
+  var spanL = document.getElementById("spanL");
+  var spanR = document.getElementById("spanR");
+  var t = 0;
+  var j = 0;
+  function fnimg(t) {
+    box.style.left = -t*1226 + "px";
+    nums[t].style.cssText = "background:grey";
+  }
+  fnimg(0);
+  function fnli(t) {
+    j = t;
+  }
+  fnli(0);
+  spanL.onclick = function() {
+    clearInterval(time);
+    if (t < 1) {
+      t = imgwidth.length - 1;
+      fnimg(t);
+      fnli(t);
+    } else {
+      t--;
+      fnli(t);
+      fnimg(t); 
+    }
+    spanL.onmouseout = function() {
+      auto();
+      nums[t].style.cssText = "background:none";
+    }
+
+  }
+  spanR.onclick = function() {
+    clearInterval(time);
+    if(t == imgwidth.length - 1) {
+      t = 0;
+      fnimg(t);
+      fnli(t);
+    } else {
+      t++;
+      fnli(t);
+      fnimg(t);
+    }
+    spanR.onmouseout = function() {
+      auto();
+      nums[t].style.cssText = "background:none";
     }
   }
-  nums[j].onlick = function() {
-    clearInterval(time);
-    t = this.index;
-    box.style.left = -t*1226 + "px";
-    i = t;
-    auto();
-    consolo.log(i);
+
+  for (var k = 0 ; k < nums.length; k++) {
+    nums[k].index = k;
+    nums[k].onclick = function() {
+      fnimg(this.index);
+      fnli(this.index);
+      nums[this.index].style.cssText = "background:grey"; 
+    }
+    nums[k].onmouseout = function() {
+      nums[this.index].style.cssText = "background:none";
+    }
   }
 }
